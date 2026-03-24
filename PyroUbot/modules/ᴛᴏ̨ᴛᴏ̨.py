637e0438f847899f255b14986c7157a1d76df61b
IlhamIlham123 <ilhamilham7667ham@gmail.com>
2026-03-24 15:32:17 +0800

    Add files via upload

---
+++ PyroUbot/modules/ᴛᴏ̨ᴛᴏ̨.py
@@ -0,0 +1,11 @@
+from PyroUbot import *
+
+MODULE = "ᴛǫᴛᴏ"
+HELP = """
+{bot.me.mention}
+
+⎆ Special Thanks:
+ᚗ Arya (Gua Selaku Own)
+ᚗ All Buyyer Ubot
+ᚗ Teman-teman yang selalu support dan buyer-buyer ku
+"""
